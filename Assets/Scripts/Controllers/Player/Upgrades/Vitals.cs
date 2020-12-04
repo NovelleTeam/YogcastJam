82464b22615f2fecc2b186165c42b6c1af1b8584
Rikocar <richard.mrvar@gmail.com>
2020-12-05 00:53:20 +0100

    Created the Vitals class.

---
+++ Assets/Scripts/Controllers/Player/Upgrades/Vitals.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vitals : MonoBehaviour
+{
+	[SerializeField]
+	private float maxHealth;
+	public float CurHealth { get; private set; }
+	public bool IsDead => CurHealth <= 0;
+
+	public event Action onDeath;
+	public event Action onTakeDamage;
+
+	public void TakeDamage(int health)
+	{
+		if (health < 0)
+		{
+			throw new System.Exception("Vitals.TakeDamage used to heal! Use Vitals.HealDamage instead.");
+		}
+
+		CurHealth -= health;
+
+		onTakeDamage?.Invoke();
+
+		if (IsDead)
+		{
+			CurHealth = 0;
+			onDeath?.Invoke();
+		}
+	}
+
+	public void HealDamage(int health)
+	{
+		if (health < 0)
+		{
+			throw new System.Exception("Vitals.HealDamage used to damage! Use Vitals.TakeDamage instead.");
+		}
+
+		CurHealth += health;
+	}
+}
