c3c3467b6bb4ea6418ebcb1ff39aca00c31ce05c
TaeKwonZeus <dyatlovleonid@gmail.com>
2020-12-04 22:41:40 +0300

    Cleaned up the PlayerMovement script

---
+++ Assets/Scripts/Controllers/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Controllers.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+    
+        private void Awake()
+        {
+            _playerInput = new PlayerInput();
+        }
+        
+        private void OnEnable()
+        {
+            _playerInput.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerInput.Disable();
+        }
+    }
+}
