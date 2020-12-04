31159ef0cadb3d86ddc985426d4c84d9db16810a
nikinov <novellenicholas@gmail.com>
2020-12-05 00:29:16 +0100

    MovementFromStandardAssets

---
+++ Assets/Scripts/Controllers/Interactive/interactiveBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactiveBox : interactiveObject
+{
+    public interactiveBox()
+    {
+        IsTakeAble = true;
+    }
+    public override void Interact()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Controllers/Interactive/interactiveObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class interactiveObject : MonoBehaviour
+{
+    public bool IsTakeAble { get; protected set; }
+    public interactiveObject()
+    {
+        IsTakeAble = true;
+    }
+
+    public virtual void Interact()
+    {
+        
+    }
+}
