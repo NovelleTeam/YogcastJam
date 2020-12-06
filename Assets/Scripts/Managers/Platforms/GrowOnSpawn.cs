0ce599a27db534e6307c3d905c1c37e69b7a3883
cbmcruz <cbmcruz.31@gmail.com>
2020-12-06 12:06:09 -0700

    Spawning big platform.

---
+++ Assets/Scripts/Managers/Platforms/GrowOnSpawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GrowOnSpawn : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+        transform.DOScale(1.0f, 1.0f).SetEase(Ease.Linear);
+    }
+}
