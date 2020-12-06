a86f6a3bcf772f97b0be21c62230b0d1b9ab8e85
Sorpirit <50134463+Sorpirit@users.noreply.github.com>
2020-12-06 10:08:21 +0200

    Pathfinding

---
+++ EnemyController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float attackDistance;
+    [SerializeField] private float startChasingDistance;
+    [SerializeField] private EnemyDestanetionSeter destanetionSeter;
+
+}
---
+++ EnemyDestanetionSeter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyDestanetionSeter : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private NavMeshAgent _agent;
+
+
+    public Transform Target
+    {
+        get => target;
+        set => target = value;
+    }
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(target != null)
+            UpdatePosition();
+    }
+    
+    private void UpdatePosition()
+    {
+        _agent.SetDestination(target.position);
+    }
+}
